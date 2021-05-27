80a3a79ad8a7f4db8c12ac2e83be38466106f0bb
kamox <kamilcc@mail.ru>
2021-05-27 18:23:18 +0600

    added theme, fonts, pushing new screen

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -1,27 +1,47 @@
 import 'package:flutter/material.dart';
+import './category_meals_screen.dart';
 
 class CategoryItem extends StatelessWidget {
   final String title;
   final Color color;
 
   CategoryItem(this.title, this.color);
+  void selectCategory(BuildContext ctx) { //* going to the next screen
+    Navigator.of(ctx).push( //! there are different methods instead of push
+      MaterialPageRoute(
+        builder: (_) {
+          return CategoryMealsScreen(); //! the page to go where
+        },
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(15),
-      child: Text(title),
-      //color: color,
-      decoration: BoxDecoration(
-        gradient: LinearGradient( // * gradient
-          colors: [
-            color.withOpacity(0.7),
-            color,
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+    return InkWell(
+      onTap: () => selectCategory(context),
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(
+          15), //! should match with border radius below (in container), for coolness
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        child: Text(
+          title,
+          style: Theme.of(context).textTheme.title,
         ),
-        borderRadius: BorderRadius.circular(15),
+        //color: color,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            // * gradient
+            colors: [
+              color.withOpacity(0.7),
+              color,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+          borderRadius: BorderRadius.circular(15),
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/category_item.dart';
 import './categories_screen.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'DeliMeals',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: CategoriesScreen()/*MyHomePage(),*/ //* defines root screen of an app
-    );
+        title: 'DeliMeals',
+        theme: ThemeData(
+          primarySwatch: Colors.orange,
+          accentColor: Colors.orange.shade600,
+          canvasColor:
+              Color.fromRGBO(255, 254, 229, 1), //TODO google what is this
+          fontFamily: 'Raleway',
+          textTheme: ThemeData.light().textTheme.copyWith(
+                //TODO google what is this
+                body1: TextStyle(
+                  color: Color.fromRGBO(20, 51, 51, 1),
+                ),
+                body2: TextStyle(
+                  color: Color.fromRGBO(20, 51, 51, 1),
+                ),
+                title: TextStyle(
+                  fontSize: 20,
+                  fontFamily: 'RobotoCondensed',
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+        ),
+        home:
+            CategoriesScreen() /*MyHomePage(),*/ //* defines root screen of an app
+        );
   }
 }
 
