dea66f4f9bac277ae9a10bf55fb4369247f13108
kamox <kamilcc@mail.ru>
2021-05-29 15:14:09 +0600

    added meal items and its showing in category

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:meals_app/category_item.dart';
-import './category_meals_screen.dart';
-import './categories_screen.dart';
+import 'screens/category_meals_screen.dart';
+import 'screens/categories_screen.dart';
 
 void main() => runApp(MyApp());
 
