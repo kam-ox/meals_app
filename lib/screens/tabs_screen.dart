b801fbfbed2280ac31a90a3aea802397d173722d
kamox <kamilcc@mail.ru>
2021-06-06 10:36:40 +0600

    added bottom navigation bar at the bottom

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -8,34 +8,53 @@ class TabsScreen extends StatefulWidget {
 }
 
 class _TabsScreenState extends State<TabsScreen> {
+  final List<Map<String, Object>> _pages = [
+    //! or just use List<Widget> if don't want to make additional changes
+    //* List of tabs pages
+    {
+      'page': CategoriesScreen(),
+      'title': 'Categories',
+    },
+    {
+      'page': FavoritesScreen(),
+      'title': 'Your Favorites',
+    }
+  ];
+  int _selectedPageIndex = 0; //* needs to change screen via selected index
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index; //* changes selected index when onTap executes
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      // initialIndex: 0, default value is 0
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Meals'),
-          bottom: TabBar(
-            indicatorColor: Colors.yellow[600],
-            indicatorSize: TabBarIndicatorSize.label,
-            indicatorWeight: 5,
-            tabs: [
-              Tab(
-                icon: Icon(Icons.category),
-                text: 'Categories',
-              ),
-              Tab(
-                icon: Icon(Icons.star),
-                text: 'Favorites',
-              ),
-            ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_pages[_selectedPageIndex]['title']),
+      ),
+      body: _pages[_selectedPageIndex]['page'],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap:
+            _selectPage, //* changing selected index. index is passed automatically by flutter, so don't need to add it manually
+        backgroundColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.white70,
+        selectedItemColor: Colors.white,
+        currentIndex: _selectedPageIndex,
+        selectedFontSize: 15,
+        //type: BottomNavigationBarType.shifting, kinda animation
+
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.category),
+            title: Text('Categories'),
           ),
-        ),
-        body: TabBarView(children: [
-          CategoriesScreen(),
-          FavoritesScreen(),
-        ]),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.star),
+            title: Text('Favorites'),
+          ),
+        ],
       ),
     );
   }
